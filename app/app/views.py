bf9a124f12174740d6f90020ee9f4234afb6949b
Adrian Maulani <takiya2888157@gmail.com>
2023-12-09 16:51:44 +0700

    adds view to Login, Dashboard and NotFound

--- app/app/views.py
+++ app/app/views.py
@@ -4,10 +4,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+class Login(View):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        return render(
+            request=request,
+            template_name="login.html"
+        )
+
+
 class Dashboard(View):
-    def get(self, request: HttpRequest) -> HttpResponse:
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         return render(
             request=request,
             template_name="dashboard.html",
             context={'test': "Hello World"}
+        )
+
+
+class NotFound(View):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        return render(
+            request=request,
+            template_name="404_not_found.html"
         )
