63af9ba00ae73530046b1779875b4e176801d7a0
Adrian Maulani <takiya2888157@gmail.com>
2023-12-14 22:44:42 +0700

    Adds middleware for authentication and route delegations

--- app/app/settings.py
+++ app/app/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.middleware.AuthMiddleware'
 ]
 
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
