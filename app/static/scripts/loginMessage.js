5f3291b665301f5c29efa229e1369d0df31c9e1e
lutfir2019 <lutfirrahman2019@gmail.com>
2023-12-14 20:51:56 +0700

    logout button

---
+++ app/static/scripts/loginMessage.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var messageElement = document.getElementById('error');
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var loginStatus = urlParams.get('login_status');
+    var registrationStatus = urlParams.get('register_status');
+
+    if (loginStatus === "WRONG_PASSWORD") {
+        messageElement.textContent = "Wrong password. Please try again.";
+    } else if (loginStatus === "BAD") {
+        messageElement.textContent = "Either it's server's fault or you entered a dirty data. Please try again.";
+    } else if (loginStatus === "USER_DOES_NOT_EXIST") {
+        messageElement.textContent = "The User you entered, is not registered in the database.";
+    } else if (registrationStatus == "BAD") {
+        messageElement.textContent = "Registration failed. Please fill the data properly and try again.";
+    } else if (registrationStatus == "OK") {
+        messageElement.classList.add("text-green-500")
+        messageElement.textContent = "Registration successful. Please login with said account.";
+    }
+    setTimeout(() => {
+        messageElement.style.display = 'none';
+    }, 5000); // 5000 milidetik (5 detik)
+});
