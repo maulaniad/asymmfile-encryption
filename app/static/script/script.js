61d9c8670c443fdc38f9c6daa4268d11acdc3ecf
Adrian Maulani <takiya2888157@gmail.com>
2023-12-14 08:43:46 +0700

    Add timeout to message display for 5 seconds

---
+++ app/static/script/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Temukan elemen dengan ID 'message'
+    var messageElement = document.getElementById('error');
+
+    // Periksa apakah parameter login_status ada dalam URL dan bernilai WRONG_PASSWORD
+    var urlParams = new URLSearchParams(window.location.search);
+    var loginStatus = urlParams.get('login_status');
+    var registrationStatus = urlParams.get('registration_status');
+
+    if (loginStatus === "WRONG_PASSWORD") {
+        // Set teks pada elemen
+        messageElement.textContent = "Wrong password. Please try again.";
+    } else if (loginStatus === "BAD") {
+        messageElement.textContent = "Either it's server's fault or you entered a dirty data. Please try again.";
+    } else if (loginStatus === "USER_DOES_NOT_EXIST") {
+        messageElement.textContent = "The User you entered, is not registered in the database.";
+    } else if (registrationStatus == "BAD") {
+        messageElement.textContent = "Registration failed. Please fill the data properly and try again.";
+    } else if (registrationStatus == "OK") {
+        messageElement.classList.add("text-green-500")
+        messageElement.textContent = "Registration successful. Please login with said account.";
+    }
+    setTimeout(() => {
+        messageElement.style.display = 'none';
+    }, 5000); // 3000 milidetik (3 detik)
+});
