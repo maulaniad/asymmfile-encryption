93ba3b249d1b43eba07b0afac1958cfdfb6083a8
Adrian Maulani <takiya2888157@gmail.com>
2023-12-14 17:04:28 +0700

    Extends username field validation

--- app/data/forms.py
+++ app/data/forms.py
@@ -3,12 +3,12 @@ from django import forms
 # Create your forms here.
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=256)
+    password = forms.CharField(widget=forms.PasswordInput())
 
 
 class RegisterForm(forms.Form):
     email = forms.CharField(max_length=256)
     fullname = forms.CharField(max_length=50)
     username = forms.CharField(max_length=25)
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput())
