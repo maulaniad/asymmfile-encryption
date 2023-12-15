67e138bef7f44566a3c116adefbccf0380c497ff
lutfir2019 <lutfirrahman2019@gmail.com>
2023-12-15 10:24:29 +0700

    logout button and modal view

--- app/static/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function logOut() {
-    fetch('/logout/', {
-        method: 'DELETE',
-    })
-        .then(response => response.text()
-            .then(res => console.log(res)))
-        .catch(err => console.log(err))
-};
