eade555d0657cdab3eeac8397742c13633e8e9f0
Adrian Maulani <maulani.ad@gmail.com>
2024-06-15 06:13:52 +0700

    feat!(data_entry): all new entry mechanism

--- app/data_entry/views.py
+++ app/data_entry/views.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from json import loads
 from typing import Any
 
-from django.http import HttpRequest
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.views import View
 
 from database.models import Data, FormatData
@@ -12,14 +12,14 @@ from helpers.types import FormatDataEntryPayload
 # Create your views here.
 
 class DataEntry(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
         format_data_query = FormatData.objects.filter(end_date__gte=datetime.now())
         data_query = Data.objects.filter(
             end_date__gte=datetime.now(),
             format__in=format_data_query
         )
 
-        data_formats: list[dict[str, Any]] = []
+        data_format_list: list[dict[str, Any]] = []
 
         for format in format_data_query:
             fields: list[dict[str, Any]] = loads(format.fields)
@@ -35,20 +35,45 @@ class DataEntry(View):
                         key: value
                     })
 
-            data_formats.append(format_dict)
+            data_format_list.append(format_dict)
+
+        data_list = [
+            {
+                'id': data.id,
+                'name': data.name,
+                'format': data.format.id,
+                'begin_date': data.begin_date.strftime('%Y-%m-%d'),
+                'end_date': data.end_date.strftime('%Y-%m-%d'),
+                'data': loads(data.data)
+            } for data in data_query
+        ]
 
         context = {
-            'data_formats': data_formats,
-            'data': data_query
+            'data_formats': data_format_list,
+            'data': data_list
         }
         return render(request, 'data_entry.html', context=context)
 
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        post_data = request.POST.copy()
+        post_data.pop('csrfmiddlewaretoken')
+
+        format_id = post_data.pop('format')
+        format_data = FormatData.objects.get(id=format_id[0])
+
+        Data.objects.create(
+            format=format_data,
+            data=[post_data],
+        )
+
+        return redirect(to="data_entry:data_entry")
+
 
 class FormatDataEntry(View):
-    def get(self, request: HttpRequest, *args, **kwargs):
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         return render(request, 'data_entry_format.html')
 
-    def post(self, request: HttpRequest, *args, **kwargs):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         data: FormatDataEntryPayload = loads(request.body)
 
         FormatData.objects.create(
@@ -56,4 +81,4 @@ class FormatDataEntry(View):
             fields=data['fields']
         )
 
-        return render(request, 'data_entry.html')
+        return redirect(to="data_entry:data_entry")
