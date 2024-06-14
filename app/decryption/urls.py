f67c7cd1c081792456d4bcb6bfecedee30916e19
Adrian Maulani <maulani.ad@gmail.com>
2024-06-14 17:12:24 +0700

    feat!(decryption): changed old encryption and decryption module

--- app/decryption/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from decryption.views import Decryption
-
-
-urlpatterns = [
-    path("", Decryption.as_view(), name="decryption")
-]
--- app/encryption/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from encryption.views import Encryption
-
-
-urlpatterns = [
-    path("", Encryption.as_view(), name="encryption")
-]
