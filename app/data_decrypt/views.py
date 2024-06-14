a0d5e30ff073d5362ac81f944a3fa7bff0b108b5
Adrian Maulani <maulani.ad@gmail.com>
2024-06-15 06:14:36 +0700

    feat!(data_decrypt): all new decrypt mechanism (WIP)

--- app/data_decrypt/views.py
+++ app/data_decrypt/views.py
@@ -1,8 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
 
 class DataDecrypt(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        return render(request, 'data_decrypt.html')
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         return render(request, 'data_decrypt.html')
