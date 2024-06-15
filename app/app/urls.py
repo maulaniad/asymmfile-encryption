0dcea420b3ee45833b92e5c1e064e77783a1fdff
Adrian Maulani <maulani.ad@gmail.com>
2024-06-15 09:17:47 +0700

    feat(data_master): download pdf

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,8 @@ from app.views import (SessionCorrector,
                        Register,
                        Logout,
                        Dashboard,
-                       MasterData)
+                       MasterData,
+                       DownloadPDF)
 
 
 urlpatterns = [
@@ -33,6 +34,7 @@ urlpatterns = [
 
     path("dashboard/", Dashboard.as_view(), name="dashboard"),
     path("data/", MasterData.as_view(), name="master_data"),
+    path("data/download/<int:format_id>/", DownloadPDF.as_view(), name="download_pdf"),
     path("data/entry/", include(("data_entry.urls", "data_entry"), namespace="data_entry")),
     path("data/decrypt/", include(("data_decrypt.urls", "data_decrypt"), namespace="data_decrypt")),
     path("rsa/encrypt/", include(("rsa_encrypt.urls", "rsa_encrypt"), namespace="rsa_encrypt")),
--- app/app/views.py
+++ app/app/views.py
@@ -1,13 +1,19 @@
 from datetime import datetime
+from io import BytesIO
+from json import loads
+from typing import Any
 
 from django.db.models import Q
 from django.contrib.auth.hashers import make_password, check_password
-from django.http import HttpRequest, HttpResponse
-from django.views import View
+from django.http import HttpRequest, HttpResponse, FileResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.views import View
+
+from weasyprint import HTML
 
 from database.forms import LoginForm, RegisterForm
-from database.models import User
+from database.models import User, FormatData, Data
 
 # Create your views here.
 
@@ -110,7 +116,88 @@ class Dashboard(View):
 
 class MasterData(View):
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        return render(
-            request=request,
-            template_name="master_data.html",
+        format_id = request.GET.get('format', None)
+
+        data_formats = FormatData.objects.filter(
+            end_date__gte=datetime.now()
         )
+
+        if not format_id:
+            context = {'data_formats': data_formats}
+            return render(request=request, template_name="master_data.html", context=context)
+
+        data = Data.objects.filter(
+            end_date__gte=datetime.now(),
+            format=data_formats.get(id=format_id)
+        )
+
+        selected_format = {
+            key: value for key, value in
+            data_formats.get(id=format_id).__dict__.items()
+            if key not in ['_state']
+        }
+
+        fields_json: list[dict[str, Any]] = loads(selected_format['fields'])
+        selected_format['fields'] = []
+        for field in fields_json:
+            for key, _ in field.items():
+                selected_format['fields'].append(key)
+
+        data_list = []
+        for x in data:
+            x_json: list[dict[str, Any]] = loads(x.data)
+            for y in x_json:
+                data_list.append(y)
+
+        context = {
+            'format_id': format_id,
+            'data_formats': data_formats,
+            'selected_format': selected_format,
+            'data': data_list
+        }
+        return render(request=request, template_name="master_data.html", context=context)
+
+
+class DownloadPDF(View):
+    def get(self, request: HttpRequest, *args, **kwargs) -> FileResponse | HttpResponse:
+        format_id = kwargs.get('format_id', None)
+
+        data_formats = FormatData.objects.filter(end_date__gte=datetime.now())
+        data = Data.objects.filter(
+            end_date__gte=datetime.now(),
+            format=data_formats.get(id=format_id)
+        )
+
+        selected_format = {
+            key: value for key, value in
+            data_formats.get(id=format_id).__dict__.items()
+            if key not in ['_state']
+        }
+
+        fields_json: list[dict[str, Any]] = loads(selected_format['fields'])
+        selected_format['fields'] = []
+        for field in fields_json:
+            for key, _ in field.items():
+                selected_format['fields'].append(key)
+
+        data_list = []
+        for x in data:
+            x_json: list[dict[str, Any]] = loads(x.data)
+            for y in x_json:
+                data_list.append(y)
+
+        context = {
+            'format_id': format_id,
+            'data_formats': data_formats,
+            'selected_format': selected_format,
+            'data': data_list,
+            'date': datetime.now()
+        }
+        html = render_to_string(template_name="pdf/download.html", context=context)
+        pdf = HTML(string=html).write_pdf(resolution=300)
+
+        if not pdf:
+            return HttpResponse('Error generating PDF')
+
+        pdf_file = BytesIO(pdf)
+        return FileResponse(pdf_file, as_attachment=True, filename="download_encrypted.pdf")
