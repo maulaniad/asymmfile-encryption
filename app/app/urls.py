05e95836754f0bcc79eb0fde09fb9e148e19194a
Adrian Maulani <maulani.ad@gmail.com>
2024-06-16 23:54:46 +0700

    fix(download): change params to 2

--- app/app/urls.py
+++ app/app/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
 
     path("dashboard/", Dashboard.as_view(), name="dashboard"),
     path("data/", MasterData.as_view(), name="master_data"),
-    path("data/download/<int:format_id>/", DownloadPDF.as_view(), name="download_pdf"),
+    path("data/download/<int:format_id>/<str:key>/", DownloadPDF.as_view(), name="download_pdf"),
     path("data/entry/", include(("data_entry.urls", "data_entry"), namespace="data_entry")),
     path("data/decrypt/", include(("data_decrypt.urls", "data_decrypt"), namespace="data_decrypt")),
     path("rsa/encrypt/", include(("rsa_encrypt.urls", "rsa_encrypt"), namespace="rsa_encrypt")),
