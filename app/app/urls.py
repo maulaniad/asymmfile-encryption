168c729637d943ce2ee387143bcd766f81751d83
Adrian Maulani <maulani.ad@gmail.com>
2024-06-19 10:11:51 +0700

    feat(download): add new download file

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,7 +22,8 @@ from app.views import (SessionCorrector,
                        Logout,
                        Dashboard,
                        MasterData,
-                       DownloadPDF)
+                       GeneratePDF,
+                       DownloadFile)
 
 
 urlpatterns = [
@@ -34,9 +35,10 @@ urlpatterns = [
 
     path("dashboard/", Dashboard.as_view(), name="dashboard"),
     path("data/", MasterData.as_view(), name="master_data"),
-    path("data/download/<int:format_id>/<str:key>/", DownloadPDF.as_view(), name="download_pdf"),
+    path("data/generate-pdf/<int:format_id>/<str:key>/", GeneratePDF.as_view(), name="generate_pdf"),
     path("data/entry/", include(("data_entry.urls", "data_entry"), namespace="data_entry")),
     path("data/decrypt/", include(("data_decrypt.urls", "data_decrypt"), namespace="data_decrypt")),
     path("rsa/encrypt/", include(("rsa_encrypt.urls", "rsa_encrypt"), namespace="rsa_encrypt")),
     path("rsa/decrypt/", include(("rsa_decrypt.urls", "rsa_decrypt"), namespace="rsa_decrypt")),
+    path("download/<str:filename>/", DownloadFile.as_view(), name="download"),
 ]
